7166c6bf4f1e44f924a419313a6b5ce2336d1605
Maximiliano Bertacchini <maxiberta@gmail.com>
2022-08-25 19:27:00 +0000

    Clear global state and reload talisker's configuration between tests

--- pytest_talisker/fixtures.py
+++ pytest_talisker/fixtures.py
@@ -4,6 +4,8 @@ import talisker
 
 @pytest.fixture(scope="session", autouse=True)
 def talisker_ctx():
-    """Ensure that all tests run under a talisker test context"""
+    """Ensure that all tests run under a clean talisker test context & environment"""
+    talisker.testing.clear_all()
+    talisker.get_config.update()
     with talisker.testing.TestContext() as ctx:
         yield ctx
